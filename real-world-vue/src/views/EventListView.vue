5d690537b6cba34653caaaf65c5acf795501a101
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2026-02-04 10:32:10 +0100

    .

--- real-world-vue/src/views/EventListView.vue
+++ real-world-vue/src/views/EventListView.vue
@@ -1,5 +1,20 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
+
+const events = ref(null)
+
+onMounted(() => {
+  axios
+  .get('http://my-json-server.typicode.com/latifcodess/294-VueComposition/events')
+  .then((response) => {
+    events.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
 </script>
 
 <template>
